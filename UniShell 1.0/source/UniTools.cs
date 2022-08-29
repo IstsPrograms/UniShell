00621c604ea4ea21ea9d56d777db81f2f6d46c18
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2022-08-29 17:41:00 +0300

    Add files via upload

---
+++ UniShell 1.0/source/UniTools.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniTools
+{
+    public class UniTools
+    {
+        
+        public string UniPathCreate(string paths, string defaultPath)
+        {
+            string output = "";
+
+            string[] _temp = paths.Split("/");
+            try
+            {
+                for (int i = 0; i < _temp.Length - 2; i++)
+                {
+                    output += _temp[i];
+                    output += "/";
+                }
+            }
+            catch
+            {
+                output = "C:/";
+            }
+            if(output.Length <= 2)
+            {
+                output = "C:/";
+            }
+
+            return output;
+        }
+    }
+
+
+
+}
