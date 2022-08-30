9f5818bdef3dae535797d8f8a217e25d740bfb52
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2022-08-30 11:45:38 +0300

    Add files via upload

---
+++ UniShell 1.1/sources/Sys.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sys
+{
+    public class Sys
+    {
+        protected string shell;
+        protected string shellVersion;
+        protected string coreVersion;
+        protected string updatesThread;
+
+
+        public Sys(string shell, string shellVersion,string coreVersion, string updatesThread)
+        {
+            this.shell = shell;
+            this.shellVersion = shellVersion;
+            this.coreVersion = coreVersion;
+            this.updatesThread = updatesThread;
+        }
+
+        public void See()
+        {
+            Console.WriteLine($"##         ##       SH: {shell}");
+            Console.WriteLine($"##         ##       SHV: {shellVersion}");
+            Console.WriteLine($"##         ##       CV: {coreVersion}");
+            Console.WriteLine($"##         ##       UT: {updatesThread}");
+            Console.WriteLine($"##         ##");
+            Console.WriteLine($"##         ##");
+            Console.WriteLine($"\\#\\       /#/");
+            Console.WriteLine($" \\#########/ ");
+        }
+    }
+}
