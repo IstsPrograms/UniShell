95f28d3460994e9b3c4332da8ccd6a890a12b96d
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2022-12-04 13:12:07 +0300

    Update Program.cs

--- UniShell 2.4 Little Fix/source/Program.cs
+++ UniShell 2.4 Little Fix/source/Program.cs
@@ -3,26 +3,7 @@ using Cmds;
 using Hydrium;
 using Services;
 using ExecuteEngines;
-string cPath; 
-if(Environment.OSVersion.Platform == PlatformID.Win32NT)
-{
-    cPath = $"C:/Users/{Environment.UserName}/";
-}
-else if(Environment.OSVersion.Platform == PlatformID.Unix)
-{
-    if(Environment.UserName == "root")
-    {
-        cPath = $"/{Environment.UserName}/";
-    }
-    else
-    {
-        cPath = $"/home/{Environment.UserName}";
-    }
-}
-else
-{
-    cPath = "Crash: Other Platform";
-}
+string cPath = $"C:/Users/{Environment.UserName}/";
 Environment.CurrentDirectory = cPath;
 Engine.OutputSysNE("[PCONF] Path Configured");
 Delegate[] cmds = { CommandsR.cd, CommandsR.dir, CommandsR.cls, CommandsR.ping, CommandsR.shutdown, CommandsR.fget, CommandsR.sys, CommandsR.echo, CommandsR.getd, CommandsR.to, CommandsR.mkzip, CommandsR.unzip, CommandsR.rm, CommandsR.write, CommandsR.mkdir, CommandsR.mkfile, CommandsR.help, CommandsR.contacts, CommandsR.ls, CommandsR.clear, CommandsR.about};
