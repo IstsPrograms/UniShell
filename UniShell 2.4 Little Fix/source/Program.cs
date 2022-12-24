928b6edc29b462e21b69f0903d791d71fd53ad79
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2022-12-24 15:50:02 +0300

    Update Program.cs

--- UniShell 2.4 Little Fix/source/Program.cs
+++ UniShell 2.4 Little Fix/source/Program.cs
@@ -9,6 +9,6 @@ Engine.OutputSysNE("[PCONF] Path Configured");
 Delegate[] cmds = { CommandsR.cd, CommandsR.dir, CommandsR.cls, CommandsR.ping, CommandsR.shutdown, CommandsR.fget, CommandsR.sys, CommandsR.echo, CommandsR.getd, CommandsR.to, CommandsR.mkzip, CommandsR.unzip, CommandsR.rm, CommandsR.write, CommandsR.mkdir, CommandsR.mkfile, CommandsR.help, CommandsR.contacts, CommandsR.ls, CommandsR.clear, CommandsR.about};
 Action[] services = { };
 Delegate[] engines = { Engines.exe, Engines.bat};
-InvokingManagerRealtime invokeMgr = new InvokingManagerRealtime(cmds, services, engines);
-await invokeMgr.MainCmd();
+Operture operture = new Operture(cmds, services, engines);
+await operture.MainCmd();
 Console.ReadKey();
