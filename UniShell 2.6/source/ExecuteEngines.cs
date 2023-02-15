c532f2d20374fd3535b7891c2aefcb896695f302
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2023-02-15 20:36:47 +0300

    Add files via upload

---
+++ UniShell 2.6/source/ExecuteEngines.cs
@@ -0,0 +1,34 @@
+﻿using Hydrium;
+using System.Diagnostics;
+namespace ExecuteEngines
+{
+    public class Engines
+    {
+        public static void exe()
+        {
+            string appToStart = Environment.GetEnvironmentVariable("runApp");
+            if(Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                Process.Start($"{Environment.CurrentDirectory}\\{appToStart}");
+                Engine.OutputSysNE("[EXE] Executing Started");
+            }
+            else
+            {
+                Engine.OutputSysWE("[EXE] Incompatible platform");
+            }
+        }
+        public static void bat()
+        {
+            string scriptToExecute = Environment.GetEnvironmentVariable("runApp");
+            if(Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                Process.Start($"{Environment.CurrentDirectory}\\{scriptToExecute}");
+                Engine.OutputSysNE("[BAT] Script executing started!");
+            }
+            else
+            {
+                Engine.OutputSysWE("[BAT] Incompatible platform");
+            }
+        }
+    }
+}
