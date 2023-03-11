441f27eeab4f99b16b23636f60c563f9082675ed
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2023-03-11 19:14:40 +0300

    Add files via upload

---
+++ UniShell 2.7/source/Logger.cs
@@ -0,0 +1,16 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Logs
+{
+    public class Logger
+    {
+        string log;
+        public string fileToLog;
+        public Logger() { log = String.Empty; fileToLog = $"C:\\Users\\{Environment.UserName}\\Documents\\log.{DateTime.Now.Day}.{DateTime.Now.Hour}.{DateTime.Now.Minute}.txt"; }
+        public void Log(string message)
+        {
+            log += $"{message}\n";
+        }
+        public void WriteToFile() { File.WriteAllText(fileToLog, log); }
+    }
+}
---
+++ UniShell 2.7/source/UniPluginBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Hydrium;
+
+namespace UniPluginBase
+{
+    class Plugin
+    {
+        public string Name { get; set; }
+        public Assembly assembly;
+        public Plugin(string Name, string File)
+        {
+            this.Name = Name.Split('.')[0];
+
+            if(Name.Contains(".dll"))
+            {
+                try
+                {
+                    Engine.OutputSysWW($"[Plugin] <{this.Name}> : loading plugin");
+                    assembly = Assembly.LoadFrom(File);
+                }
+                catch
+                {
+                    Engine.OutputSysWE($"[Plugin] <{this.Name}> : non UniShell plugin");
+                }
+            }
+
+        }
+        public void StartUp()
+        {
+            try
+            {
+                Type type = assembly.GetType("Plugin.PluginClass");
+                object instance = Activator.CreateInstance(type);
+                MethodInfo method = instance.GetType().GetMethod("StartUp");
+                method.Invoke(instance, null);
+            }
+            catch 
+            {
+                Engine.OutputSysWE($"[Plugin] <{Name}> : non UniShell plugin");
+            }
+        }
+    }
+}
